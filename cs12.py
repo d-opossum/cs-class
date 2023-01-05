31c78465d00a0ffd95c6eb5cc451258fd09984db
d-opossum <117316565+d-opossum@users.noreply.github.com>
2023-01-04 20:05:49 -0800

    how

--- cs12.py
+++ cs12.py
@@ -16,3 +16,9 @@ print(name)
 print(loc)
 print(tele)
 # -----------------------
+# The how
+# variable = "data value"
+# print() 
+# Will always read out or see out the value inside in this case the variable
+
+
