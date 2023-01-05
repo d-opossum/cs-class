61f7266f5eef6f437a4ecc199e8b5673e3dae195
d-opossum <117316565+d-opossum@users.noreply.github.com>
2023-01-04 20:31:54 -0800

    Update cs12.py

--- cs12.py
+++ cs12.py
@@ -6,15 +6,25 @@
 # -----------------------
 # Define variables
 # -----------------------
-name, loc, tele = "Ken","\nVirginia","\n555-555-0101"
+name, loc, tele = "Ken","Virginia","555-555-0101"
 # -----------------------
 # Print out variables
 # -----------------------
-print(name, loc, tele)
+print(name)
+print(loc)
+print(tele)
 # -----------------------
 # The how
 # variable = "data value"
 # print() 
 # Will always read out or see out the value inside in this case the variable
-
+# -----------------------
+# Other Possible
+# print("Ken","\nVirginia","\n555-555-0101")
+# -----------------------
+# Other Possible
+# name, loc, tele = "Ken","\nVirginia","\n555-555-0101"
+# print(name, loc, tele)
+# -----------------------
+# -----------------------
 
