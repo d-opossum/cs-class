27515d02e99409d9c083aae2fb4c25445ec43dde
d-opossum <117316565+d-opossum@users.noreply.github.com>
2023-01-07 19:13:22 -0800

    updated

--- cs15.py
+++ cs15.py
@@ -1,19 +1,15 @@
 # Do not copy | My Personal Use Only 
 # Originally on Replit
 # -------------------------------------
-# Input
-# Enter the base: 5
-# Enter the height: 7
-# -------------------------------------
-base = int(input("Enter the base: "))
-height = int(input("Enter the height: "))
-# -------------------------------------
-# Calculation
-# -------------------------------------
-area = ((base * height) / 2)
-# -------------------------------------
-# Print Area
-# The area is 17.5 square units.
-# -------------------------------------
-print ("The area is:", area, "square units.")
-# -------------------------------------
+def userinput():
+  base = float(input("Enter the base: "))
+  height = float(input("Enter the height: "))
+  return base, height
+def area_cal(base, height):
+  area = ((base * height) / 2)
+  return area
+def output_area(area):
+  print ("The area is: %a square units." % area)
+base, height = userinput()
+area = area_cal(base, height)
+output_area(area)
