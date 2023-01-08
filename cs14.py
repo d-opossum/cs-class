24677a7cba6a86ef4a30ce896d06ee02a6604726
d-opossum <117316565+d-opossum@users.noreply.github.com>
2023-01-07 19:29:32 -0800

    Update cs14.py

--- cs14.py
+++ cs14.py
@@ -4,15 +4,14 @@
 def area_calculation(width, height):
   area = width * height
   return area
-# -----
 def area_output(area):
   print("The area is %a square units." % area)
-# -----
 def measure_input():
     width = int(input("Enter the width: "))
     height = int(input("Enter the height: "))
     return width, height
-# -----
-width, height = measure_input()
-area = area_calculation(width, height)
-area_output(area)
+def ending():
+    width, height = measure_input()
+    area = area_calculation(width, height)
+    area_output(area)
+ending()
