d9f8f67fabe0a40fa9cffd89b84a9a1afa20e2d5
d-opossum <117316565+d-opossum@users.noreply.github.com>
2023-01-07 19:38:42 -0800

    updated

--- cs15.py
+++ cs15.py
@@ -5,11 +5,13 @@ def userinput():
   base = float(input("Enter the base: "))
   height = float(input("Enter the height: "))
   return base, height
-def area_cal():
-  base, height = userinput()
+def area_cal(base, height):
   area = ((base * height) / 2)
   return area
-def output_area():
-  area = str(area_cal())
+def output_area(area):
   print ("The area is: %a square units." % area)
-output_area()
+def ending():
+  base, height = userinput()
+  area = str(area_cal(base, height))
+  output_area(area)
+ending()
