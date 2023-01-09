4a138f09c6e9dd0be8a742dbd2ba89e78c48ee86
d-opossum <117316565+d-opossum@users.noreply.github.com>
2023-01-08 17:35:30 -0800

    done badly

--- cs37.py
+++ cs37.py
@@ -1,13 +1,16 @@
-# INCOMPLETE
 #Enter the starting salary: $30000
 #Enter the annual % increase: 2
 #Enter the number of years: 10
-
+import decimal
 starting_salary = int(input("Enter the starting salary: "))
 percentage_inc = float(input("Enter the annual % increase: "))
 number_years = int(input("Enter the number of years: "))
 print("\nYear   Salary\n-------------")
-for number_years in range (number_years):
-  num = (str(number_years+1) + "    " +'$'+ str (starting_salary))
+deced = decimal.Decimal(str(starting_salary))
+num_base = str(1) + "    " +'$'+ str (deced.quantize(decimal.Decimal('.01'), decimal.ROUND_HALF_DOWN)) + '\n'
+print(num_base)
+for number_years in range(1,number_years):
+  starting_salary = (starting_salary + (starting_salary*percentage_inc)/100)
+  deced = decimal.Decimal(str(starting_salary))
+  num = (str(number_years+1) + "    " +'$'+ str (deced.quantize(decimal.Decimal('.01'), decimal.ROUND_HALF_DOWN)) + '\n')
   print(num)
-  starting_salary = round((starting_salary + (starting_salary*percentage_inc)/100), 2)
